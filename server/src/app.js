c178099ee0f838ce85b4171c760e16c5cc0c3108
Joel Catalano <joelcata56@gmail.com>
2026-09-04 00:40:34 -0300

    seguridad para el envio de email

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,19 @@ const cors = require('cors');
 const app = express();
 
 // Middlewares globales
-app.use(cors({ origin: '*' }));
+const corsOptions = {
+  origin: [
+    'http://localhost:5173', 
+    'http://localhost:5174', // Por si levantás el otro front localmente
+    'https://fluxer-web.vercel.app', 
+    'https://fluxerapp.cloud', 
+    'https://www.fluxerapp.cloud'
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
